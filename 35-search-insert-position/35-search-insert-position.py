547b23510bf4e37f48b806c655964e7de1512d3d
Devanshu Desai <46750877+DevanshuDesai15@users.noreply.github.com>
2022-09-30 10:47:57 -0700

    Time: 83 ms (56.26%), Space: 14.7 MB (82.49%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,17 +1,19 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         # log(n)
-        l,r = 0, len(nums) -1
+        l,r = 0, len(nums) -1 # left and right pointer
         
         while l<=r:
             mid = (l+r) //2
             
-            if target == nums[mid]:
+            if target == nums[mid]: # check first
                 return mid
             
-            if target > nums[mid]:
-                l = mid +1
-            else:
-                r = mid -1
-            
-        return l
+            if target > nums[mid]: # if target is big then middle 
+                l = mid +1 # start search to right
+            else: # target is less then middle
+                r = mid -1 # start search left
+             
+        return l # never found a target 
+    # Why left?? not right
+    # if left crosses right then assign left index
