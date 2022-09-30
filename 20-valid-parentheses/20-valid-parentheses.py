4911f209b6368e64f5f2909a3341bb5f80cb1905
Devanshu Desai <46750877+DevanshuDesai15@users.noreply.github.com>
2022-09-30 11:58:47 -0700

    Time: 64 ms (21.30%), Space: 13.9 MB (72.28%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -4,12 +4,13 @@ class Solution:
         closeToOpen = { ")" : "(", "]" : '[', "}" : "{" }
         
         for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
-                    stack.pop()
+            if c in closeToOpen: # if is a closing parantheses
+                if stack and stack[-1] == closeToOpen[c]: # stack is not empty
+                    # stack[-1] is last value added in stack
+                    stack.pop() 
                 else:
                     return False
-            else:
+            else: # if we not get closing parantheses
                 stack.append(c)
-                
+        # after going throight every value stack can only return true if stack is empty
         return True if not stack else False
